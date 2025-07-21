bddc810674fe0542c8a5b6684c743f445649d98c
akhila <akhila.gangisetty001@gmail.com>
2025-07-21 20:58:46 +0530

    feat: defined local schema with 1 byte slice for event name

--- projects/poap_cert/smart_contracts/poap_contract/contract.py
+++ projects/poap_cert/smart_contracts/poap_contract/contract.py
@@ -9,3 +9,15 @@ class POAPCertificateApp(Application):
             App.localPut(receiver.get(), Bytes("event_name"), event_name.get()),
             Approve()
         )
+
+app = POAPCertificateApp()
+
+app_spec = ApplicationSpec(
+    app=app,
+    schema=ApplicationStateSchema(
+        local_ints=0,
+        local_bytes=1,
+        global_ints=0,
+        global_bytes=0
+    )
+)
