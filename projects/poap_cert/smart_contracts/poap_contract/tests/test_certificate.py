7a4a31bb31118ec1050d2388d7ff9229acd39dc9
akhila <akhila.gangisetty001@gmail.com>
2025-07-21 21:08:16 +0530

    v

--- projects/poap_cert/smart_contracts/poap_contract/tests/test_certificate.py
+++ projects/poap_cert/smart_contracts/poap_contract/tests/test_certificate.py
@@ -10,3 +10,22 @@ def app_client():
 def test_deploy(app_client):
     app_client.create()
     assert app_client.app_id != 0
+
+    def test_create_and_get_certificate(app_client):
+    user = app_client.app_creator
+
+    # Call create_certificate method
+    app_client.call(
+        "create_certificate",
+        receiver=user.address,
+        event_name="Hack Series 2025"
+    )
+
+    # Call get_certificate to fetch it
+    result = app_client.call(
+        "get_certificate",
+        receiver=user.address
+    )
+
+    assert result.return_value == "Hack Series 2025"
+
