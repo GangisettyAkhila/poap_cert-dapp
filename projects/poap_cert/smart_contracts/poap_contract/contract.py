a0715421dd0d8b3957347bb84f7cb732548ccc09
akhila <akhila.gangisetty001@gmail.com>
2025-09-14 19:32:55 +0530

    refactor: Consolidate and correct the smart contract

--- projects/poap_cert/smart_contracts/poap_contract/contract.py
+++ projects/poap_cert/smart_contracts/poap_contract/contract.py
@@ -1,46 +1,84 @@
-from beaker import Application, external, ApplicationSpec, ApplicationStateSchema
+
+from beaker import Application, external, create, Authorize
 from pyteal import *
 
-class POAPCertificateApp(Application):
+class PoapCert(Application):
+    """
+    Proof of Attendance Protocol (POAP) Certificate Contract
+    """
 
-    @external
-    def create_certificate(self, receiver: abi.Address, event_name: abi.String):
+    # Global State
+    cert_counter = ApplicationStateValue(
+        stack_type=TealType.uint64, default=Int(0)
+    )
+
+    # Mappings
+    cert_recipient = Mapping(TealType.uint64, TealType.bytes)
+    cert_event = Mapping(TealType.uint64, TealType.bytes)
+    cert_valid = Mapping(TealType.uint64, TealType.uint64)
+
+    @create
+    def create(self):
+        return self.initialize_application_state()
+
+    @external(authorize=Authorize.only_creator())
+    def issue_cert(
+        self, recipient: abi.Address, event: abi.String, *, output: abi.Uint64
+    ):
         return Seq(
-            App.localPut(receiver.get(), Bytes("event_name"), event_name.get()),
-            Approve()
+            (cert_id := ScratchVar()).store(self.cert_counter.get()),
+            self.cert_counter.set(self.cert_counter + Int(1)),
+            self.cert_recipient[cert_id.load()].set(recipient.get()),
+            self.cert_event[cert_id.load()].set(event.get()),
+            self.cert_valid[cert_id.load()].set(Int(1)),
+            output.set(cert_id.load()),
         )
 
-    #  New method: Read the certificate event name
     @external(read_only=True)
-    def get_certificate(self, receiver: abi.Address, *, output: abi.String):
-        return Seq(
-            output.set(App.localGet(receiver.get(), Bytes("event_name"))),
-            Approve()
+    def verify_cert(self, cert_id: abi.Uint64, *, output: abi.String):
+        return If(
+            self.cert_valid[cert_id.get()] == Int(1),
+            output.set(self.cert_event[cert_id.get()]),
+            output.set(Bytes("Invalid or Revoked")),
         )
 
-#  Instantiate app and set schema
-app = POAPCertificateApp()
+    @external(authorize=Authorize.only_creator())
+    def revoke_cert(self, cert_id: abi.Uint64):
+        return self.cert_valid[cert_id.get()].set(Int(0))
 
-app_spec = ApplicationSpec(
-    app=app,
-    schema=ApplicationStateSchema(
-        local_ints=0,
-        local_bytes=1,
-        global_ints=0,
-        global_bytes=0
-    )
-)
+    @external(read_only=True)
+    def get_certificate(self, recipient: abi.Address, *, output: abi.String):
+        # This is a simplified implementation. A real-world app would need a more robust way
+        # to look up certificates by recipient, as a recipient could have multiple certificates.
+        # This implementation will only find the last certificate issued to a recipient.
+        return output.set(self.cert_event[self.get_cert_id_by_recipient(recipient.get())])
 
-@external(read_only=True)
-def get_all_certificates(self, *, output: abi.String):
-    return Seq(
-        output.set("Feature not yet implemented"),
-        Approve()
-    )
+    @external(read_only=True)
+    def has_certificate(self, recipient: abi.Address, *, output: abi.Bool):
+        return output.set(
+            self.get_cert_id_by_recipient(recipient.get()) != Int(0)
+        )
+    
+    @external
+    def delete_certificate(self, cert_id: abi.Uint64):
+        return self.cert_valid[cert_id.get()].set(Int(0))
 
-@external(read_only=True)
-def has_certificate(self, receiver: abi.Address, *, output: abi.Bool):
-    return Seq(
-        output.set(App.localGet(receiver.get(), Bytes("event_name")) != Bytes("")),
-        Approve()
-    )
+    # Internal method to find a certificate ID by recipient address.
+    # Note: This is a simplified and inefficient way to do this.
+    # A more robust solution would involve a different data structure.
+    @internal(TealType.uint64)
+    def get_cert_id_by_recipient(self, recipient: TealType.bytes):
+        i = ScratchVar(TealType.uint64)
+        return For(
+            i.store(Int(0)),
+            i.load() < self.cert_counter,
+            i.store(i.load() + Int(1)),
+        ).Do(
+            If(
+                self.cert_recipient[i.load()] == recipient,
+                Return(i.load()),
+            )
+        )
+        Return(Int(0))
+
+app = PoapCert()
